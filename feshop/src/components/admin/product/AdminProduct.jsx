8347dacc02257ddabc86e3dd7f498550aaed47f1
HaDucDev <haductrung392000@gmail.com>
2023-02-10 16:55:26 +0700

    update-ui-1

--- feshop/src/components/admin/product/AdminProduct.jsx
+++ feshop/src/components/admin/product/AdminProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button} from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import DataTable from "react-data-table-component";
 import 'bootstrap/dist/css/bootstrap.css';
 import convert_vi_to_en from "../../../utils/utils";
@@ -10,15 +10,21 @@ const AdminProduct = () => {
     const [productList, setProductList] = useState([]);// state datatable
     const [intiText, setText] = useState("");// state search
 
+    const [showAddModal, setShowAddModal] = useState(false);// state bat/tat modal create
     const [load, setLoadTable] = useState(false);// state load khi them thanh cong
+    const [value, setValue] = useState("");// state supplier ban dau khi modal add
+    const [isSubmitting, setIsSubmitting] = useState(false);// state nut chi nhan dc mot lan
+    const [isLoading, setIsLoading] = useState(false);// tao spiner quay de biet data dang gui, cham 1 ti
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const [selectedCategoryId, setSelectedCategoryId] = useState(0);
 
     const search = (data) => {
         return data.filter(row => convert_vi_to_en(row.productName.toLowerCase()).indexOf(convert_vi_to_en(intiText.toLowerCase())) > -1
             || row.productId.toString().includes(intiText)
             || convert_vi_to_en(row.category.categoryName.toLowerCase()).indexOf(convert_vi_to_en(intiText.toLowerCase())) > -1
             || convert_vi_to_en(row.supplier.supplierName.toLowerCase()).indexOf(convert_vi_to_en(intiText.toLowerCase())) > -1
-            || row.unitPrice.toString().includes(intiText)
-            );
+            || row.unitPrice.toString().includes(intiText));
     }
 
     useEffect(() => {
@@ -51,13 +57,13 @@ const AdminProduct = () => {
             selector: (row) => <img width={50} height={50} src={row.productImage} alt="lỗi ảnh" />,
             center: true
         }
-        , 
+        ,
         {
             name: "Loại hàng",
             selector: row => row.category.categoryName,
             center: true
         }
-        ,{
+        , {
             name: "Hãng",
             selector: row => row.supplier.supplierName,
             center: true
@@ -107,13 +113,178 @@ const AdminProduct = () => {
                         <Button style={{ marginLeft: "10px" }} variant="outline-dark"
                             onClick={() => {
                                 // setErrorResponse({ supplierName: "" });
-                                // setShowAddModal(true)
+                                setShowAddModal(true)
                             }}>THÊM</Button>
                     </>
                 }
                 subHeaderAlign="right"
                 customStyles={customStyles}
             />
+
+            {/* Modal them supplier*/}
+            <Modal show={showAddModal} onHide={() => setShowAddModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Thêm supplier</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Tên sản phẩm</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Nhập tên sản phẩm"
+                            onChange={(e) => {
+                                (setValue(e.target.value));
+                                // setErrorResponse({
+                                //     supplierName: ""
+                                // })
+                                setIsSubmitting(false);// mo nut
+                            }}
+                        />
+                        {/* <ValidationMessage
+                            errorResponse={errorResponse}
+                            field="supplierName" /> */}
+                    </Form.Group>
+                    <div style={{ display: "flex" }}>
+
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Số lượng</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Nhập số lượng"
+                                onChange={(e) => {
+                                    (setValue(e.target.value));
+                                    // setErrorResponse({
+                                    //     supplierName: ""
+                                    // })
+                                    setIsSubmitting(false);// mo nut
+                                }}
+                            />
+                            {/* <ValidationMessage
+                            errorResponse={errorResponse}
+                            field="supplierName" /> */}
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ position: "absolute", right: "15px" }}>
+                            <Form.Label>Giảm giá</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Giảm giá"
+                                onChange={(e) => {
+                                    (setValue(e.target.value));
+                                    // setErrorResponse({
+                                    //     supplierName: ""
+                                    // })
+                                    setIsSubmitting(false);// mo nut
+                                }}
+                            />
+                            {/* <ValidationMessage
+                            errorResponse={errorResponse}
+                            field="supplierName" /> */}
+                        </Form.Group>
+                    </div>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Giá gốc</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Nhập tên hãng"
+                            onChange={(e) => {
+                                (setValue(e.target.value));
+                                // setErrorResponse({
+                                //     supplierName: ""
+                                // })
+                                setIsSubmitting(false);// mo nut
+                            }}
+                        />
+                        {/* <ValidationMessage
+                            errorResponse={errorResponse}
+                            field="supplierName" /> */}
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Đặc tả</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Nhập tên hãng"
+                            onChange={(e) => {
+                                (setValue(e.target.value));
+                                // setErrorResponse({
+                                //     supplierName: ""
+                                // })
+                                setIsSubmitting(false);// mo nut
+                            }}
+                        />
+                        {/* <ValidationMessage
+                            errorResponse={errorResponse}
+                            field="supplierName" /> */}
+                    </Form.Group>
+                    <div style={{ display: "flex" }}>
+
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Chọn loại hàng</Form.Label>
+                            <Form.Control
+                                as="select"
+                                name="categoryId"
+                                value={selectedCategoryId}
+                                onChange={(e) => {
+                                    (setSelectedCategoryId(e.target.value));
+                                    // setErrorResponse({
+                                    //     supplierName: ""
+                                    // })
+                                    setIsSubmitting(false);// mo nut
+                                }}>
+                                <option value="">Chọn category...</option>
+
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ position: "absolute", right: "15px" }}>
+                            <Form.Label>Tên hãng</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Chọn hãng"
+                                onChange={(e) => {
+                                    (setValue(e.target.value));
+                                    // setErrorResponse({
+                                    //     supplierName: ""
+                                    // })
+                                    setIsSubmitting(false);// mo nut
+                                }}
+                            />
+                        </Form.Group>
+                    </div>
+
+
+
+
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                        <Form.Label>Chọn ảnh cho hãng</Form.Label>
+                        <Form.Control
+                            type="file"
+                            onChange={(e) => {
+                                (setSelectedFile(e.target.files[0]));
+                            }}
+                        />
+                    </Form.Group>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowAddModal(false)}>
+                        Đóng
+                    </Button>
+
+                    <Button variant="outline-primary" disabled={isSubmitting}
+                        style={{ zIndex: "1" }}
+                        onClick={{}}>
+                        {/* {isLoading && (
+                            <Spinner
+                                style={{ margin: "auto", zIndex: "9" }}
+                                animation="border"
+                                variant="primary"
+                            />
+                        )} */}
+                        Lưu
+                    </Button>
+
+
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
