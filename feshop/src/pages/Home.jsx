7dc7c0974a89e9bf416915c76fa5bbdcd6cb1e6f
HaDucDev <haductrung392000@gmail.com>
2023-02-15 18:05:45 +0700

    paginaton-ui

--- feshop/src/pages/Home.jsx
+++ feshop/src/pages/Home.jsx
@@ -2,25 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { Card, Button, Image } from 'react-bootstrap';
 import './css/home.css';
 import productServiceUser from "../services/user/user.product.service";
-
+import Pagination from 'react-pagination-library';
+import 'react-pagination-library/build/css/index.css';
 const Home = () => {
 
 
-    const [listProductHome, setListProductHome] = useState([]) 
+    const [listProductHome, setListProductHome] = useState([])
 
-    useEffect(()=>{
-       productServiceUser.getAllHomeProductService().then((responseData)=>{
-        setListProductHome(responseData.data.content)
-       }).catch(error => alert("Lỗi " + error + ". Bạn hãy quay lại sau."));
-    },[])
-   
+    useEffect(() => {
+        productServiceUser.getAllHomeProductService().then((responseData) => {
+            setListProductHome(responseData.data.content)
+        }).catch(error => alert("Lỗi " + error + ". Bạn hãy quay lại sau."));
+    }, [])
+
+    const handlePageChange = (pageNumber) => {
+        console.log(`active page is ${pageNumber}`);
+      };
+
 
     return (
         <>
             <div className="card-list-container" >
                 {listProductHome.map((item) => (
                     <Card className="card-container">
-                        <Image src={item.productImage} fluid className="card-image" style={{ borderBottom: '2px solid #ddd' }}/>
+                        <Image src={item.productImage} fluid className="card-image" style={{ borderBottom: '2px solid #ddd' }} />
                         <Card.Body>
                             <Card.Title className="card-title">{item.productName}</Card.Title>
                             <Card.Text className="card-text">{item.unitPrice} VND</Card.Text>
@@ -29,6 +34,13 @@ const Home = () => {
                     </Card>
                 ))}
             </div>
+            <Pagination
+                currentPage={3}
+                totalPages={10}
+                showFirstLastPages
+                size="md"
+                changeCurrentPage={handlePageChange}         
+            />
         </>
     );// ngoac tong
 }
