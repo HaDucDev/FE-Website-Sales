b6328bd44763f88ce8d9dc4b0d8f8ac6e1dd7776
HaDucDev <haductrung392000@gmail.com>
2023-03-07 11:08:45 +0700

    many-color

--- feshop/src/components/admin/statistical/AdminStatistical.jsx
+++ feshop/src/components/admin/statistical/AdminStatistical.jsx
@@ -10,9 +10,24 @@ const AdminStatistical = () => {
         [4, 5.5],
         [11, 14],
         [4, 5],
-        [3, 3.5],
+        [36, 3.5],
         [6.5, 7]
       ];
+
+      const data1 = [
+        ['Year', 'Sales', 'Expenses', 'Profit'],
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350],
+      ];
+
+      const options = {
+        title: 'Company Performance',
+        hAxis: { title: 'Year', titleTextStyle: { color: '#333' } },
+        vAxis: { minValue: 0 },
+        colors: ['#FF0000', '#00FF00', '#0000FF'], // Cấu hình màu sắc cho 3 cột đầu tiên
+      };
 
 
     return (
@@ -23,12 +38,8 @@ const AdminStatistical = () => {
                 height={500}
                 chartType="ColumnChart"
                 loader={<div>Loading Chart</div>}
-                data={data}
-                options={{
-                    title: "Doanh thu theo loại sản phẩm",
-                    // Just add this option
-                    is3D: true,
-                }}
+                data={data1}
+                options={options}
                 rootProps={{ "data-testid": "2" }}
             />
         </>
