a42ceeacbec599ebfac9af8d46eac9fb39c86ff6
HaDucDev <haductrung392000@gmail.com>
2023-03-07 11:28:29 +0700

    test-done-color-random-col

--- feshop/src/components/admin/statistical/AdminStatistical.jsx
+++ feshop/src/components/admin/statistical/AdminStatistical.jsx
@@ -1,35 +1,24 @@
 import Chart from "react-google-charts";
-
+import React from "react";
 
 const AdminStatistical = () => {
 
 
+    const generateColor = () => {//lay mau ngau nhien
+        let r = parseInt(Math.random() * 255);
+        let g = parseInt(Math.random() * 255);
+        let b = parseInt(Math.random() * 255);
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+
     const data = [
-        ["age", "weight"],
-        [8, 12],
-        [4, 5.5],
-        [11, 14],
-        [4, 5],
-        [36, 3.5],
-        [6.5, 7]
-      ];
-
-      const data1 = [
-        ['Year', 'Sales', 'Expenses', 'Profit'],
-        ['2014', 1000, 400, 200],
-        ['2015', 1170, 460, 250],
-        ['2016', 660, 1120, 300],
-        ['2017', 1030, 540, 350],
-      ];
-
-      const options = {
-        title: 'Company Performance',
-        hAxis: { title: 'Year', titleTextStyle: { color: '#333' } },
-        vAxis: { minValue: 0 },
-        colors: ['#FF0000', '#00FF00', '#0000FF'], // Cấu hình màu sắc cho 3 cột đầu tiên
-      };
-
-
+        ["name", "money", { role: "style" }],
+        ["John", 1000, generateColor()],
+        ["Mary", 2000, generateColor()],
+        ["Tom", 1500, generateColor()],
+        ["Lucy", 3000, generateColor()],
+        ["Bob", 1200, generateColor()],
+    ];
     return (
         <>Trang Thong Ke
 
@@ -38,12 +27,18 @@ const AdminStatistical = () => {
                 height={500}
                 chartType="ColumnChart"
                 loader={<div>Loading Chart</div>}
-                data={data1}
-                options={options}
+                data={data}
+                options={{
+                    title: "Doanh thu theo loại sản ph",
+                    // Just add this option
+                    is3D: true,
+                    //colors: colors,
+                }}
                 rootProps={{ "data-testid": "2" }}
             />
         </>
     )
+
 }
 
 export default AdminStatistical;
