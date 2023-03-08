daf251c34140c9fe96e69961f26e69ef032679e0
HaDucDev <haductrung392000@gmail.com>
2023-03-08 17:04:34 +0700

    done-ui-navbar-home

--- feshop/src/components/user/home/HomeHeader.jsx
+++ feshop/src/components/user/home/HomeHeader.jsx
@@ -1,12 +1,59 @@
-
-import Header from "./Header";
+import { useNavigate } from "react-router-dom";
 import "./home-header.css";
 
 const HomeHeader = () => {
-   
+
+    const nav = useNavigate();
+
     return (
         <>
-           <Header/>
+            <nav class="navbar">
+                <ul class="navbar-menu">
+                    <li class="navbar-item">
+                        <a href="/" class="navbar-link navbar-logo">Trang chủ</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="/" class="navbar-link">Danh mục</a>
+                        <ul class="navbar-submenu">
+                            <li class="navbar-submenu-item" style={{ width: "100px" }}>
+                                Danh mục 1
+                                <ul class="navbar-submenu-item-submenu">
+                                    <li class="navbar-submenu-item" style={{ width: "200px" }} onClick={()=>{nav("/login")}}>
+                                        Danh mục 1 Nhà cung cấp
+                                    </li>
+                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                        Danh mục 1 Nhà cung cấp
+                                    </li>
+                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                        Danh mục 1 Nhà cung cấp
+                                    </li>
+                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                        Danh mục 1 Nhà cung cấp
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="navbar-submenu-item" style={{ width: "100px" }}>
+                                Danh mục 2
+                                <ul class="navbar-submenu-item-submenu">
+                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                        Danh mục 2 Nhà cung cấp
+                                    </li>
+                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                        Danh mục 2 Nhà cung cấp
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="/policy" class="navbar-link">Chính sách bán hàng</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a href="/contact" class="navbar-link">Liên hệ</a>
+                    </li>
+                </ul>
+            </nav>
+
         </>
     );
 };
