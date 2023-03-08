d642ae5adfa6193fec9e042d57838a395812da56
HaDucDev <haductrung392000@gmail.com>
2023-03-08 08:55:47 +0700

    ui-2

--- feshop/src/components/user/home/HomeHeader.jsx
+++ feshop/src/components/user/home/HomeHeader.jsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
 import "./home-header.css"
+
+
 const HomeHeader = () => {
+
+
+    const [hovered, setHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHovered(true);
+      };
+    
+      const handleMouseLeave = () => {
+        setHovered(false);
+      };
 
     return (
         <>
-            <ul>132312
-                <li>312312</li>
-                <li>312312</li>
-                <ul>132312
-                    <li>dasdasd</li>
-                    <li>asddasdsa</li>
-                </ul>
-            </ul>
+
+            <div className="main-menu-navbar">
+                <div className="child-div-of-main">Trang chủ</div>
+                <div className="child-div-of-main" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+                    <ul>Sản phẩm
+                        {
+                            hovered && (
+                                <>
+                                <li className="filler-menu">312312</li>
+                        <li className="filler-menu">312312</li>
+                        <ul className="filler-menu">132312
+                            <li className="filler-menu">dasdasd</li>
+                            <li className="filler-menu">asddasdsa</li>
+                        </ul>
+                                </>
+                            )
+                        }
+                        
+                    </ul>
+                </div>
+                <div className="child-div-of-main">Chính sách</div>
+                <div className="child-div-of-main">Liên hệ</div>
+            </div>
+
         </>
     )
 
