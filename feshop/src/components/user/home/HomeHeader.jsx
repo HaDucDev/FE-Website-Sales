f14e65c31ed6f33cfda888abd7d7f57bac3d5db6
HaDucDev <haductrung392000@gmail.com>
2023-03-08 21:51:17 +0700

    onKeyDown

--- feshop/src/components/user/home/HomeHeader.jsx
+++ feshop/src/components/user/home/HomeHeader.jsx
@@ -2,11 +2,20 @@ import { useNavigate } from "react-router-dom";
 import "./home-header.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react";
 const HomeHeader = () => {
 
+    const [inputValue, setInputValue] = useState("");// value thanh tim kiem
     const nav = useNavigate();
 
     let ok = 1
+
+    const handleSendText = (e)=>{
+        if(e.key==='Enter'){
+            console.log(inputValue);
+            
+        }
+    }
 
     return (
         <>
@@ -15,54 +24,57 @@ const HomeHeader = () => {
                     type="text"
                     className="search-input"
                     placeholder="Tìm kiếm sản phẩm..."
+                    onChange={(e)=>{setInputValue(e.target.value)}}
+                    //onKeyPress={handleKeyPress}
+                    onKeyDown={handleSendText}
                 />
                 <button className="search-btn">
                     <FontAwesomeIcon icon={faSearch} />
                 </button>
             </div>
-            <nav class="navbar">
-                <ul class="navbar-menu">
-                    <li class="navbar-item">
-                        <a href="/" class="navbar-link navbar-logo">Trang chủ</a>
+            <nav className="navbar">
+                <ul className="navbar-menu">
+                    <li className="navbar-item">
+                        <a href="/" className="navbar-link navbar-logo">Trang chủ</a>
                     </li>
-                    <li class="navbar-item">
-                        <a href="/" class="navbar-link">Danh mục</a>
-                        <ul class="navbar-submenu">
-                            <li class="navbar-submenu-item" style={{ width: "100px" }}>
+                    <li className="navbar-item">
+                        <a href="/" className="navbar-link">Danh mục</a>
+                        <ul className="navbar-submenu">
+                            <li className="navbar-submenu-item" style={{ width: "100px" }}>
                                 Danh mục 1
-                                <ul class="navbar-submenu-item-submenu">
-                                    <li class="navbar-submenu-item" style={{ width: "200px" }} onClick={() => { nav(`/login?ma=${ok}`) }}>
+                                <ul className="navbar-submenu-item-submenu">
+                                    <li className="navbar-submenu-item" style={{ width: "200px" }} onClick={() => { nav(`/login?ma=${ok}`) }}>
                                         Danh mục 1 Nhà
                                     </li>
-                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                    <li className="navbar-submenu-item" style={{ width: "200px" }}>
                                         Danh mục 1 Nhà cung cấp
                                     </li>
-                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                    <li className="navbar-submenu-item" style={{ width: "200px" }}>
                                         Danh mục 1 Nhà cung cấp
                                     </li>
-                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                    <li className="navbar-submenu-item" style={{ width: "200px" }}>
                                         Danh mục 1 Nhà cung cấp
                                     </li>
                                 </ul>
                             </li>
-                            <li class="navbar-submenu-item" style={{ width: "100px" }}>
+                            <li className="navbar-submenu-item" style={{ width: "100px" }}>
                                 Danh mục 2
-                                <ul class="navbar-submenu-item-submenu">
-                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                <ul className="navbar-submenu-item-submenu">
+                                    <li className="navbar-submenu-item" style={{ width: "200px" }}>
                                         Danh Nhà cung cấp
                                     </li>
-                                    <li class="navbar-submenu-item" style={{ width: "200px" }}>
+                                    <li className="navbar-submenu-item" style={{ width: "200px" }}>
                                         Danh mục 2 Nhà cung cấp
                                     </li>
                                 </ul>
                             </li>
                         </ul>
                     </li>
-                    <li class="navbar-item">
-                        <a href="/policy" class="navbar-link">Chính sách bán hàng</a>
+                    <li className="navbar-item">
+                        <a href="/policy" className="navbar-link">Chính sách bán hàng</a>
                     </li>
-                    <li class="navbar-item">
-                        <a href="/contact" class="navbar-link">Liên hệ</a>
+                    <li className="navbar-item">
+                        <a href="/contact" className="navbar-link">Liên hệ</a>
                     </li>
                 </ul>
             </nav>
